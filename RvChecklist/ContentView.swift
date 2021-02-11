7d7c0f03037cde782bd97448b4d2301ef1d37cf5
Ron <ron@lisles.net>
2021-02-11 08:54:02 -0600

    Initial Commit

---
+++ RvChecklist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RvChecklist
+//
+//  Created by Ron Lisle on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RvChecklist/RvChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  RvChecklistApp.swift
+//  RvChecklist
+//
+//  Created by Ron Lisle on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RvChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
