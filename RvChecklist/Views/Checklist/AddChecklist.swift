88c930f175d3c16050d447e7a06f8d4d5bc9e1fd
Ron <ron@lisles.net>
2021-03-21 13:50:46 -0500

    wip

---
+++ RvChecklist/Views/Checklist/AddChecklist.swift
@@ -0,0 +1,20 @@
+//
+//  AddChecklist.swift
+//  RvChecklist
+//
+//  Created by Ron Lisle on 3/18/21.
+//
+
+import SwiftUI
+
+struct AddChecklist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddChecklist_Previews: PreviewProvider {
+    static var previews: some View {
+        AddChecklist()
+    }
+}
