7f9e9e52c15623ce10c499e69aad5ba1159da0e3
Ron <ron@lisles.net>
2023-06-11 06:41:22 -0500

    wip

--- RvChecklist/Views/Checklist/ChecklistView.swift
+++ RvChecklist/Views/Checklist/ChecklistView.swift
@@ -13,44 +13,17 @@ struct ChecklistView: View {
     @Query private var checklistItems: [ChecklistItem]
     
     @State private var showCompleted = true
-    @State private var showMenu = false
+    @State private var showSettings = false
     @State private var menuSelection: String? = nil
-//    @State private var phase = "Pre-Trip"
     private var phases = ["Pre-Trip", "Departure", "Arrival"]
 
     @Environment(\.modelContext) private var modelContext
     
-//    init() {
-//        UISegmentedControl.appearance().backgroundColor = .black
-//        UISegmentedControl.appearance().selectedSegmentTintColor = .selectable
-//        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
-//        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
-//    }
-    
     var body: some View {
-        
-        NavigationView {
-
-//            GeometryReader { geometry in
-
-//                ZStack(alignment: .leading) {   // for sidemenu
-                    
+        NavigationStack {
                     VStack {
-                        
                         ChecklistHeader()
-
-//                        Picker(selection: $phase, label: Text("Phase")) {
-//                            ForEach(phases, id: \.self) {
-//                                Text($0)
-//                            }
-//                        }
-//                        .pickerStyle(SegmentedPickerStyle())
-//                        .padding(.bottom, 0)
-//                        .background(Color.black)
-
-                        // Checklist Section
                         List {
-                            
                             ForEach(phases, id: \.self) { phase in
                                 Section(header:
                                             HStack {
@@ -60,75 +33,42 @@ struct ChecklistView: View {
                                 }
                                     .padding(.vertical, 8)
                                 ) {
-                                    
-                                    
                                     if(checklistItems.category(phase).todo().count == 0) {
                                         Text("No \(phase) items found")
                                     } else {
                                         ForEach(checklistItems.category(phase).filter { isShown(item:$0) }, id: \.self) { item in
-                                            
                                             NavigationLink(destination: DetailView(listItem: item)) {
                                                 Text("\(item.title)")
                                                 //ChecklistRow(listItem: item)
                                             }
                                         }
                                     }
-                                    
                                 }
                                 .textCase(nil)
-                                
-                            }// phases
-
+                            }// foreach phase
                         } // List
                         .padding(.top, -8)
                         .listStyle(PlainListStyle())    // Changed from GroupedListStyle
                         //.animation(.easeInOut)
-
-                        
                     }//VStack
-//                    .frame(width: geometry.size.width, height: geometry.size.height)
-//                    .offset(x: self.showMenu ? geometry.size.width/2 : 0)
-//                    .disabled(self.showMenu ? true : false)
-//                    if self.showMenu {
-//                        // Close side menu (This breaks onMove
-//                        let drag = DragGesture()
-//                            .onEnded {
-//                                if $0.translation.width < -100 {
-//                                    withAnimation {
-//                                        self.showMenu = false
-//                                    }
-//                                }
-//                            }
-//
-//                        MenuView(showMenu: $showMenu,
-//                                 showCompleted: $showCompleted,
-//                                 selection: $menuSelection)
-//                            .frame(width: geometry.size.width/2)
-//                            .transition(.move(edge: .leading))
-//                        .gesture(drag)
-//
-//                    }
+                    .blackNavigation
 
-//                }//ZStack for sidemenu
-//                .blackNavigation
-//                
-//                .navigationBarTitleDisplayMode(.inline)
-//                .navigationTitle("RV Checklist")
-//                .toolbar {
-//                     ToolbarItem(placement: .navigationBarLeading) {
-//                         Button(action: {
-//                             withAnimation {
-//                                 self.showMenu.toggle()
-//                             }
-//                         }) {
-//                             Image(systemName: "line.horizontal.3")
-//                                 .imageScale(.large)
-//                         }
-//                     }
-//                }
-//            } //GeometryReader
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationTitle("RV Checklist")
+                .toolbar {
+                     ToolbarItem(placement: .navigationBarTrailing) {
+                         Button(action: {
+                             withAnimation {
+                                 self.showSettings.toggle()
+                             }
+                         }) {
+                             Image(systemName: "gear")
+                                 .imageScale(.large)
+                         }
+                     }
+                }
         }//NavigationView
-        .accentColor( .black)   // Sets back button color
+        .accentColor(.white)   // Sets back button color
 
     }
         
