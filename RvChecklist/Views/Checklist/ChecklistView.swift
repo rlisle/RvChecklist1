fb6d05bec5332f79cae6aa52dfcb52e3690e4f35
Ron <ron@lisles.net>
2023-06-11 06:47:41 -0500

    wip

--- RvChecklist/Views/Checklist/ChecklistView.swift
+++ RvChecklist/Views/Checklist/ChecklistView.swift
@@ -21,55 +21,65 @@ struct ChecklistView: View {
     
     var body: some View {
         NavigationStack {
-                    VStack {
-                        ChecklistHeader()
-                        List {
-                            ForEach(phases, id: \.self) { phase in
-                                Section(header:
-                                            HStack {
-                                    Text(phase)
-                                    Spacer()
-                                    Text("(\(checklistItems.category(phase).done().count) of \(checklistItems.category(phase).count) done)")
-                                }
-                                    .padding(.vertical, 8)
-                                ) {
-                                    if(checklistItems.category(phase).todo().count == 0) {
-                                        Text("No \(phase) items found")
-                                    } else {
-                                        ForEach(checklistItems.category(phase).filter { isShown(item:$0) }, id: \.self) { item in
-                                            NavigationLink(destination: DetailView(listItem: item)) {
-                                                Text("\(item.title)")
-                                                //ChecklistRow(listItem: item)
-                                            }
-                                        }
+            VStack {
+                ChecklistHeader()
+                List {
+                    ForEach(phases, id: \.self) { phase in
+                        Section(header:
+                                    HStack {
+                            Text(phase)
+                            Spacer()
+                            Text("(\(checklistItems.category(phase).done().count) of \(checklistItems.category(phase).count) done)")
+                        }
+                            .padding(.vertical, 8)
+                        ) {
+                            if(checklistItems.category(phase).todo().count == 0) {
+                                Text("No \(phase) items found")
+                            } else {
+                                ForEach(checklistItems.category(phase).filter { isShown(item:$0) }, id: \.self) { item in
+                                    NavigationLink(destination: DetailView(listItem: item)) {
+                                        Text("\(item.title)")
+                                        //ChecklistRow(listItem: item)
                                     }
                                 }
-                                .textCase(nil)
-                            }// foreach phase
-                        } // List
-                        .padding(.top, -8)
-                        .listStyle(PlainListStyle())    // Changed from GroupedListStyle
-                        //.animation(.easeInOut)
-                    }//VStack
-                    .blackNavigation
-
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationTitle("RV Checklist")
-                .toolbar {
-                     ToolbarItem(placement: .navigationBarTrailing) {
-                         Button(action: {
-                             withAnimation {
-                                 self.showSettings.toggle()
-                             }
-                         }) {
-                             Image(systemName: "gear")
-                                 .imageScale(.large)
-                         }
-                     }
+                            }
+                        }
+                        .textCase(nil)
+                    }// foreach phase
+                } // List
+                .padding(.top, -8)
+                .listStyle(PlainListStyle())    // Changed from GroupedListStyle
+                //.animation(.easeInOut)
+            }//VStack
+            .blackNavigation
+            
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("RV Checklist")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        withAnimation {
+                            self.showCompleted.toggle()
+                        }
+                    }) {
+                        Image(systemName: showCompleted ? "checklist" : "checklist.unchecked")
+                            .imageScale(.large)
+                    }
                 }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        withAnimation {
+                            self.showSettings.toggle()
+                        }
+                    }) {
+                        Image(systemName: "gear")
+                            .imageScale(.large)
+                    }
+                }
+            }
         }//NavigationView
         .accentColor(.white)   // Sets back button color
-
+        
     }
         
     func isShown(item: ChecklistItem) -> Bool {
